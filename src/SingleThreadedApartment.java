85e4f092b6ca649b41327e3df2691a7f3ca43d58
Minjana Ruwanpura <minjanaruwanpura@gmail.com>
2025-03-01 13:02:28 +0530

    First Commit

---
+++ src/SingleThreadedApartment.java
@@ -0,0 +1,17 @@
+public class SingleThreadedApartment {
+
+    private static String name = null;
+
+    // Sequential program because there is only one thread - single threaded application (apartment)
+
+    public static void main(String[] args) {
+
+        System.out.println("The name of the default thread is "
+                +Thread.currentThread().getName());
+
+        for(int i = 0; i < 10; i++) {
+            System.out.println(Thread.currentThread().getName()+" : "+i);
+        }
+
+    }
+}
